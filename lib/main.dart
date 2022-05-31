5c51c5ab83af76c79b1f96ac575ddb556d7c0b30
Sai Chaitanya Aluru <alurusaichaitanya@gmail.com>
2022-05-31 10:31:46 +0800

    feat: added sound playing feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void changeSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey(int buttonNumber, Color color) =>
+    Expanded(
+        child: TextButton(
+      onPressed: () {
+        changeSound(buttonNumber);
+      },
+      style: TextButton.styleFrom(
+          backgroundColor: color),
+    ));
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Container(),
+        home: Scaffold(
+      backgroundColor: Colors.black38,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                buildKey(1, Colors.blue),
+                buildKey(2, Colors.red),
+                buildKey(3, Colors.blueGrey),
+                buildKey(4, Colors.green),
+                buildKey(5, Colors.lightGreenAccent)
+              ]),
         ),
       ),
-    );
+    ));
   }
 }
